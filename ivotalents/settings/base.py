9417fc01293f7d7ba3d3cb43c3f5e0728d96e2cc
wa-p montoya <wa_p@hotmail.com>
2017-05-05 17:36:51 -0430

    recuperacion contraseña

--- ivotalents/settings/base.py
+++ ivotalents/settings/base.py
@@ -35,7 +35,7 @@ EMAIL_HOST_PASSWORD = '18815251'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 
--- ivotalents/settings/local.py
+++ ivotalents/settings/local.py
@@ -36,6 +36,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL= EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
 
